95f8f0099f4086c251177f03aefb3bcf2ca615ab
gpisa00 <giuseppepisa00@gmail.com>
2020-12-24 12:40:14 +0100

    https

--- src/services/CustomerService.js
+++ src/services/CustomerService.js
@@ -1,6 +1,5 @@
-
 export function getCustomers() {
-  return fetch('http://app-restexample.herokuapp.com/rest/customers')
+  return fetch('https://app-restexample.herokuapp.com/rest/customers')
     .then(data => data.json())
 }
 
